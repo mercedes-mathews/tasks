c2987055c8958de88ef612ca7039b8d0205ee717
mmathews <mmathews@udel.edu>
2023-03-13 19:35:37 -0400

    passed test for findQuestion

--- src/nested.ts
+++ src/nested.ts
@@ -37,7 +37,10 @@ export function findQuestion(
     questions: Question[],
     id: number
 ): Question | null {
-    return null;
+    const questionFromID = questions.find(
+        (question: Question): boolean => question.id === id
+    );
+    return questionFromID === undefined ? null : questionFromID;
 }
 
 /**
