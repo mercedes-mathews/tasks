a6e82a1689708c17da586f18889153412eeeedfc
mmathews <mmathews@udel.edu>
2023-03-13 20:17:21 -0400

    passed test for toCVS

--- src/nested.ts
+++ src/nested.ts
@@ -110,7 +110,13 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const questionCVS = questions
+        .map(
+            (question: Question): string =>
+                `${question.id},${question.name},${question.options.length},${question.points},${question.published}`
+        )
+        .join("\n");
+    return "id,name,options,points,published\n" + questionCVS;
 }
 
 /**
