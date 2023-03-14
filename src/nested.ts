5024d18b6c74dd4d5c2b980901e898543709dbe3
mmathews <mmathews@udel.edu>
2023-03-13 20:30:39 -0400

    passed test for publishedAll

--- src/nested.ts
+++ src/nested.ts
@@ -141,7 +141,10 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * each question is now published, regardless of its previous published status.
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    const allPublished = questions.map(
+        (question: Question): Question => ({ ...question, published: true })
+    );
+    return allPublished;
 }
 
 /***
