ebea346096d829ad897f51d97a4c59d464e197f4
mmathews <mmathews@udel.edu>
2023-03-14 21:58:50 -0400

    Passed tests for ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,25 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    function changeQuestionType(): void {
+        if (questionType === "multiple_choice_question") {
+            setQuestionType("short_answer_question");
+        } else {
+            setQuestionType("multiple_choice_question");
+        }
+    }
+    return (
+        <div>
+            <Button onClick={changeQuestionType}>Change Type</Button>
+            {questionType === "multiple_choice_question" ? (
+                <span>Multiple Choice</span>
+            ) : (
+                <span>Short Answer</span>
+            )}
+        </div>
+    );
 }
