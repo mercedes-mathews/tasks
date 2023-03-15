358b51f4b9bd76c937a8812af431c2ad924e49b6
mmathews <mmathews@udel.edu>
2023-03-14 22:56:44 -0400

    Passed tests for TwoDice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,33 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeftDie] = useState<number>(1);
+    const [rightDie, setRightDie] = useState<number>(4);
+
+    function leftRoll(): void {
+        setLeftDie(d6());
+    }
+    function rightRoll(): void {
+        setRightDie(d6());
+    }
+    function showResult(): string {
+        if (leftDie === rightDie) {
+            if (leftDie === 1) {
+                return "Lose";
+            } else {
+                return "Win";
+            }
+        }
+        return "";
+    }
+
+    return (
+        <div>
+            <Button onClick={leftRoll}>Roll Left</Button>
+            <Button onClick={rightRoll}>Roll Right</Button>
+            <span data-testid="left-die">Left: {leftDie} </span>
+            <span data-testid="right-die">Right: {rightDie}</span>
+            <span> Result: {showResult()}</span>
+        </div>
+    );
 }
